f03eac83f20bf27583771408b71aa08892a9ea48
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2024-01-05 19:04:28 -0300

    move useCurrentDate() to this file

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function useCurrentDate() {
+
+    var dateObj = new Date();
+    var currentDate = dateObj.toISOString().split('T')[0];
+    var date = document.querySelector("#date");
+    date.value = currentDate;
+
+}
