8503c26060b3a404f5f96f01811b0900109970c6
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2024-01-07 23:48:56 -0300

    turn group and home controllers authorized to test new login functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using ExpensesWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace ExpensesWebApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
